f2cc3565ed849c47680c5f875306e044462fcae4
pirooz193 <piroozazarabad@gmail.com>
2023-08-30 18:20:07 +0330

    initial commit

---
+++ src/main/java/com/mycompany/librarysystem/LibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.librarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySystemApplication.class, args);
+    }
+
+}
