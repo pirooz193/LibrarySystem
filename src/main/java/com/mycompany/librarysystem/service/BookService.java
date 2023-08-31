3562df3f7488d3e0cc881536cd20cffd5f30df18
pirooz193 <piroozazarabad@gmail.com>
2023-08-31 17:30:00 +0330

    Create endpoints to create and fetch books.

--- src/main/java/com/mycompany/librarysystem/service/BookService.java
+++ src/main/java/com/mycompany/librarysystem/service/BookService.java
@@ -1,4 +1,21 @@
 package com.mycompany.librarysystem.service;
 
+import com.mycompany.librarysystem.domain.Book;
+import com.mycompany.librarysystem.dto.BookDTO;
+import com.mycompany.librarysystem.dto.criteria.BookCriteria;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface BookService {
+    BookDTO createBook(BookDTO bookDTO);
+
+    BookDTO getRequiredBook(Long bookId);
+
+    Page<Book> searchBooks(BookCriteria criteria, Pageable pageable);
+
+    List<Book> getAllBooks();
+
+    BookDTO addAuthorToRequiredBook(Long bookNumber,Long authorId);
 }
