1399ad6474aefd972ab6010a19fcad241fb62638
pirooz193 <piroozazarabad@gmail.com>
2023-08-31 17:23:33 +0330

    Create class to handle constant values.

---
+++ src/main/java/com/mycompany/librarysystem/domain/Constants.java
@@ -0,0 +1,8 @@
+package com.mycompany.librarysystem.domain;
+
+public class Constants {
+
+    public static final String AUTHOR = "author ";
+    public static final String BOOK = "book ";
+    public static final String MEMBER = "member ";
+}
