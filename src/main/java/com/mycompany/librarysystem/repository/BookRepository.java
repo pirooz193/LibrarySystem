0ec0043a029f62e95d36e74088367e0837fad987
pirooz193 <piroozazarabad@gmail.com>
2023-08-31 17:26:08 +0330

    Created method to fetch required book by bookNumber.

--- src/main/java/com/mycompany/librarysystem/repository/BookRepository.java
+++ src/main/java/com/mycompany/librarysystem/repository/BookRepository.java
@@ -2,8 +2,11 @@ package com.mycompany.librarysystem.repository;
 
 import com.mycompany.librarysystem.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> , JpaSpecificationExecutor<Book> {
+
+    Book findBookByBookNumber(Long bookNumber);
 }
