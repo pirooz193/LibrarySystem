d72f151f2683e35dab7377fac5d65cf9cc6c7cb5
pirooz193 <piroozazarabad@gmail.com>
2023-08-31 17:27:46 +0330

    Create endpoints to create and fetch books.

--- src/main/java/com/mycompany/librarysystem/service/impl/BookServiceIml.java
+++ src/main/java/com/mycompany/librarysystem/service/impl/BookServiceIml.java
@@ -1,8 +1,82 @@
 package com.mycompany.librarysystem.service.impl;
 
+import com.mycompany.librarysystem.domain.Author;
+import com.mycompany.librarysystem.domain.Book;
+import com.mycompany.librarysystem.domain.Constants;
+import com.mycompany.librarysystem.dto.BookDTO;
+import com.mycompany.librarysystem.dto.criteria.BookCriteria;
+import com.mycompany.librarysystem.repository.AuthorRepository;
+import com.mycompany.librarysystem.repository.BookRepository;
 import com.mycompany.librarysystem.service.BookService;
+import com.mycompany.librarysystem.service.mapper.BookMapper;
+import com.mycompany.librarysystem.service.specifications.BookSpecifications;
+import com.mycompany.librarysystem.web.error.DuplicateBookException;
+import com.mycompany.librarysystem.web.error.NotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookServiceIml implements BookService {
+
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+    private final BookMapper bookMapper;
+
+    public BookServiceIml(BookRepository bookRepository, AuthorRepository authorRepository, BookMapper bookMapper) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+        this.bookMapper = bookMapper;
+    }
+
+    @Override
+    public BookDTO createBook(BookDTO bookDTO) {
+        Book book = bookMapper.toEntity(bookDTO);
+        book.setBorrowed(false);
+        Book savedBook = bookRepository.save(book);
+        return bookMapper.toDTO(savedBook);
+    }
+
+    @Override
+    public BookDTO getRequiredBook(Long bookId) {
+        Book requiredBook = bookRepository.findById(bookId)
+                .orElseThrow(() -> new NotFoundException(Constants.BOOK +bookId));
+        return bookMapper.toDTO(requiredBook);
+    }
+
+    public Page<Book> searchBooks(BookCriteria criteria, Pageable pageable) {
+        Specification<Book> spec = Specification.where(null);
+
+        if (criteria.getAuthorName() != null && !criteria.getAuthorName().isEmpty()) {
+            spec = spec.or(BookSpecifications.hasAuthorName(criteria.getAuthorName()));
+        }
+        if (criteria.getTranslatorName() != null && !criteria.getTranslatorName().isEmpty()) {
+            spec = spec.or(BookSpecifications.hasTranslatorName(criteria.getTranslatorName()));
+        }
+        if (criteria.getTitle() != null && !criteria.getTitle().isEmpty()) {
+            spec = spec.or(BookSpecifications.hasBookName(criteria.getTitle()));
+        }
+        return bookRepository.findAll(spec, pageable);
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public BookDTO addAuthorToRequiredBook(Long bookNumber, Long authorId) {
+        Book requiredBook = bookRepository.findById(bookNumber)
+                .orElseThrow(() -> new NotFoundException(Constants.BOOK + bookNumber));
+        Author author = authorRepository.findById(authorId)
+                .orElseThrow(() -> new NotFoundException(Constants.AUTHOR + authorId));
+        if (requiredBook.getAuthors().contains(author)) {
+            throw new DuplicateBookException(Constants.AUTHOR + authorId);
+        }
+        requiredBook.getAuthors().add(author);
+        return bookMapper.toDTO(bookRepository.save(requiredBook));
+    }
 }
--- src/main/java/com/mycompany/librarysystem/web/rest/BookController.java
+++ src/main/java/com/mycompany/librarysystem/web/rest/BookController.java
@@ -1,9 +1,51 @@
 package com.mycompany.librarysystem.web.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mycompany.librarysystem.domain.Book;
+import com.mycompany.librarysystem.dto.BookDTO;
+import com.mycompany.librarysystem.dto.criteria.BookCriteria;
+import com.mycompany.librarysystem.service.BookService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/book")
 public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<BookDTO> createBook(@RequestBody BookDTO bookDTO) {
+        BookDTO createdBook = bookService.createBook(bookDTO);
+        return ResponseEntity.created(URI.create("/api/book")).body(createdBook);
+    }
+
+    @PatchMapping("{bookId}/add-author/{authorId}")
+    public ResponseEntity<BookDTO> addAuthorToRequiredBook(@PathVariable Long bookId,@PathVariable Long authorId) {
+        BookDTO requiredBook = bookService.addAuthorToRequiredBook(bookId, authorId);
+        return ResponseEntity.ok(requiredBook);
+    }
+
+    @GetMapping("/{bookId}")
+    public ResponseEntity<BookDTO> getRequiredBook(@PathVariable Long bookId) {
+        BookDTO requiredBook = bookService.getRequiredBook(bookId);
+        return ResponseEntity.ok(requiredBook);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks(@ModelAttribute BookCriteria criteria, Pageable pageable) {
+        if (criteria == null) {
+            return ResponseEntity.ok(bookService.getAllBooks());
+        }
+        Page<Book> books = bookService.searchBooks(criteria, pageable);
+        return ResponseEntity.ok(books.getContent());
+    }
 }
