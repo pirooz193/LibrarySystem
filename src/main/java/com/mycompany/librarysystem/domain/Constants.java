014e52e5d08f812f68a4f355b6485b3a4eb44853
pirooz193 <piroozazarabad@gmail.com>
2023-09-01 09:54:29 +0330

    Create constant attributes for naming of report Excel file.

--- src/main/java/com/mycompany/librarysystem/domain/Constants.java
+++ src/main/java/com/mycompany/librarysystem/domain/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
     public static final String AUTHOR = "author ";
     public static final String BOOK = "book ";
     public static final String MEMBER = "member ";
+    public static final String REPORT = "report";
+    public static final String DASH = "-";
+    public static final String TO = "-";
 }
